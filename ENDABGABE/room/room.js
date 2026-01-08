83b8498c308eab8501bab14c055cb6e5c716e34a
jennisinger <jsi47883@stud.hs-furtwangen.de>
2026-01-08 15:18:53 +0100

    viel neu

--- ENDABGABE/room/room.js
+++ ENDABGABE/room/room.js
@@ -3,17 +3,22 @@ const arBtn = document.querySelector("#ar-button");
 
 let buttonVisible = false;
 
-// Marker erkannt → Button anzeigen
 marker.addEventListener("markerFound", () => {
   arBtn.style.display = "block";
   buttonVisible = true;
 });
-
 
 marker.addEventListener("markerLost", () => {
   arBtn.style.display = "none";
   buttonVisible = false;
 });
 
+arBtn.addEventListener("click", () => {
+  if (!buttonVisible) return;
+  window.location.href = "room.html";
+});
 
-
+arBtn.addEventListener("touchstart", () => {
+  if (!buttonVisible) return;
+  window.location.href = "room.html";
+});
