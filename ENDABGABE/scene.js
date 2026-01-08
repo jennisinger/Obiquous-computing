33cd24095cfdbd8f258b8991316a494ec421efae
jennisinger <jsi47883@stud.hs-furtwangen.de>
2026-01-08 10:36:06 +0100

    farbe

--- ENDABGABE/scene.js
+++ ENDABGABE/scene.js
@@ -3,19 +3,19 @@ const arBtn = document.querySelector("#ar-button");
 
 let buttonVisible = false;
 
-// Marker erkannt → Button anzeigen
+
 marker.addEventListener("markerFound", () => {
   arBtn.style.display = "block";
   buttonVisible = true;
 });
 
-// Marker verloren → Button ausblenden
+
 marker.addEventListener("markerLost", () => {
   arBtn.style.display = "none";
   buttonVisible = false;
 });
 
-// Klick / Touch
+
 arBtn.addEventListener("click", () => {
   if (!buttonVisible) return;
   alert("🎉 Start gedrückt!");
